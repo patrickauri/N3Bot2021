6a554ca1093c875565c17c506f2723065aa8ea1a
Patrick Auri <patrickaulie@gmail.com>
2022-05-26 18:23:59 +0200

    updated role storage

--- bot.js
+++ bot.js
@@ -1,97 +1,95 @@
 // Perform Setup
-const Discord = require('discord.js');
-require('dotenv').config();
-const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
-const token = process.env.TOKEN;
-const roles = require('./roles.json');
+const Discord = require("discord.js")
+require("dotenv").config()
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+})
+const token = process.env.TOKEN
+let roles = []
 
-// Get Role ID
-const GetRoleID = (roleName) => {
-	let found = false;
-	let result;
-	for (let i = 0; i < roles.length && found === false; i++) {
-		if (roles[i][0] === roleName) {
-			result = roles[i][1];
-			found = true;
-		}
-	}
-	return found === true ? result : null;
-};
+const FetchBotData = () => {
+  // The channel in which we pick up the bot data from
+  const dataChannel = client.channels.cache.get("977931066578989096")
 
-// Get Role Name
-const GetRoleName = (msg, role) => {
-	return msg.guild.roles.cache.find((r) => r.id === GetRoleID(role)).name;
-};
-
-// Add Role
-const AddRoleOLD = (user, role) => {
-	if (user.roles.cache.some((r) => r.id === GetRoleID(role))) {
-		RemoveRole(msg, role, mute);
-	} else {
-		user.roles.add(GetRoleID(role));
-		const roleName = msg.guild.roles.cache.find((r) => r.id === GetRoleID(role)).name;
-		if (!mute) {
-			const reply = `You've been given the role: **${roleName}**`;
-			msg.channel.send(reply);
-		}
-		console.log(`Added Role ${roleName} to user: ${msg.member.user.username}`);
-	}
-};
-
-/* const AddRole = (user, role) {
-    if (user.roles.cache.some((r) => r.id !== GetRoleID(role)))
-    user.roles.add
-}*/
+  // Fetching the raw data
+  dataChannel.messages
+    .fetch({ limit: 1 })
+    .then((msg) => {
+      let content
+      msg.forEach((m) => (content = m.content))
+      // Convert strings to array
+      let newRoles
+      newRoles = content.split("\n")
+      roles = []
+      // Remove unnecessary entries
+      newRoles.shift()
+      newRoles.shift()
+      newRoles.pop()
+      newRoles.pop()
+      newRoles.forEach((e) => {
+        let newItem = []
+        newItem[0] = e.substr(0, 2)
+        newItem[1] = e.substr(2, e.length)
+        roles.push(newItem)
+      })
+    })
+    .catch((err) => console.log(err))
+}
 
 // Client On Ready
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
-});
+client.on("ready", () => {
+  FetchBotData()
+  console.log(`Logged in as ${client.user.tag}`)
+})
 
 // Client On Message Reaction Add
-client.on('messageReactionAdd', async (reaction, user) => {
-    const member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+client.on("messageReactionAdd", async (reaction, user) => {
+  //FetchBotData()
+  console.log(reaction.emoji)
+  const member = reaction.message.guild.members.cache.find(
+    (member) => member.id === user.id
+  )
 
-    if (reaction.partial)
-    {
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error(`Couldn't fetch reaction message (${error})`);
-            return;
-        }
-    }
-
+  if (reaction.partial) {
     try {
-        const r = roles.find(e => e[0] === reaction.emoji.name);
-        member.roles.add(r[1]);
-        console.log(`Added role ${r[0]} to ${member.displayName}`);
-    } catch {
-        e => console.error(e)
+      await reaction.fetch()
+    } catch (error) {
+      console.error(`Couldn't fetch reaction message (${error})`)
+      return
     }
-});
+  }
 
-client.on('messageReactionRemove', async (reaction, user) => {
-    const member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+  try {
+    const r = roles.find((e) => e[0] === reaction.emoji.name)
+    member.roles.add(r[1])
+    console.log(`Added role ${r[1]} to ${member.displayName}`)
+  } catch {
+    ;(e) => console.error(e)
+  }
+})
 
-    if (reaction.partial)
-    {
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error(`Couldn't fetch reaction message (${error})`);
-            return;
-        }
-    }
+client.on("messageReactionRemove", async (reaction, user) => {
+  const member = reaction.message.guild.members.cache.find(
+    (member) => member.id === user.id
+  )
 
+  if (reaction.partial) {
     try {
-        const r = roles.find(e => e[0] === reaction.emoji.name);
-        member.roles.remove(r[1]);
-        console.log(`Removed role ${r[0]} to ${member.displayName}`);
-    } catch {
-        e => console.error(e)
+      await reaction.fetch()
+    } catch (error) {
+      console.error(`Couldn't fetch reaction message (${error})`)
+      return
     }
-});
+  }
+
+  try {
+    const r = roles.find((e) => e[0] === reaction.emoji.name)
+    member.roles.remove(r[1])
+    console.log(`Removed role ${r[0]} from ${member.displayName}`)
+  } catch {
+    ;(e) => console.error(e)
+  }
+})
 
 // Log in to Discord
-client.login(token);
+client.login(token)
